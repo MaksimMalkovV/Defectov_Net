76d72368b26c6fef80c602d63bcaee5932851b4a
MaksimMalkovV <maer2971@gmail.com>
2024-03-20 11:30:19 +0300

    v.0.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./app.scss";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Portfolio from "./components/portfolio/Portfolio";
 import Gallery from "./components/gallery/Gallery";
 import Services from "./components/services/Services";
 
